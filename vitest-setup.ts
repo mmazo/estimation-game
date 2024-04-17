978b7c99ee235e7e5d15e47135b63a0710a04c9d
Mazo Michail <michail.mazo@gmail.com>
2024-04-17 16:20:02 +0200

    fixed test config

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,9 +1,6 @@
-import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom/vitest";
-import { afterEach, expect } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup, configure } from "@testing-library/react";
-
-expect.extend(matchers);
 
 configure({
   asyncUtilTimeout: 2000,
